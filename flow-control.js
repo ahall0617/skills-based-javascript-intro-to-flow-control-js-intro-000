84ccae82c4e2f6bbd7dd265fc48eb0c4939f1e88
Ariana Hall <amhall4323@ung.edu>
2019-05-12 19:59:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,33 @@
 function basicTeenager(age) {
-
+if (13<=age<=19) {
+return "You are a teenager!";
+}
 }
 
 function teenager(age) {
-
+if (age<=19 && age=>13) {
+return "You are a teenager!";
+} else {return "You are not a teenager";}
 }
 
 function ageChecker(age) {
-
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+} else if (age <= 12) {
+return "You are a kid";
+} else {return "You are a grownup";}
 }
 
 function ternaryTeenager(age) {
-
+if (age >= 13 && age <= 19) {
+return "You are a teenager";
+} else {return "You are not a teenager";}
 }
 
 function switchAge(age) {
 
+}
+if (13 <= age <= 19) {"You are a teenager!"
+} else if (age < 13) {"You are not a teenager!"
+} else (age > 19) {"You are not a teenager!"
 }
